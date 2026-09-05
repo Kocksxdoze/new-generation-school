f7b6014fe17e3b4caecc959da713fe701f6817a6
Kocksxdoze <famelezzz@gmail.com>
2026-09-05 20:25:31 +0500

    Fix COOKIE_OPTIONS: sameSite none and secure for cross-site auth

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -4,8 +4,8 @@ import * as authService from "./auth.service.js";
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: env.isProduction,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days, keep in sync with JWT_EXPIRES_IN
 };
 
